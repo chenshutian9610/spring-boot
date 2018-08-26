8f0cae75cad81dd4fb5b52b2cb49f450d8093350
triski <1406448399@qq.com>
2018-08-26 21:01:26 +0800

    little change

--- src/main/java/tree/Application.java
+++ src/main/java/tree/Application.java
@@ -5,14 +5,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 @MapperScan("tree.dao")
 public class Application extends SpringBootServletInitializer {
     public static void main(String args[]){
         SpringApplication.run(Application.class,args);
     }
-
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(Application.class);
